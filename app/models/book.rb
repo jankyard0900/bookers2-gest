71d16fcdf8d8691d3a99dcbbfbde5414cced964a
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-09-21 04:59:02 +0000

    [Finish]5-3

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,11 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  has_many :favorites
+  has_many :book_comments
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
