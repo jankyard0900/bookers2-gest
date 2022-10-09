de5a492053b45d63c4de59b88358dfda1eedaccc
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-10-09 10:22:19 +0000

    [update]rubocop実行

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,7 +43,6 @@ class BooksController < ApplicationController
   end
 
   private
-
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,13 @@ class User < ApplicationRecord
 
 
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    (profile_image.attached?) ? profile_image : "no_image.jpg"
+  end
+
+  def self.guest
+    find_or_create_by!(name: "guestuser", email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
   end
 end
